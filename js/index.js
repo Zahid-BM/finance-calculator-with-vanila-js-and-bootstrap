b4aa7e5e4321daa459c1c29ee03ae058064d78d9
BM. Zahidul Islam <bmzahidulislam@outlook.com>
2022-02-17 10:10:15 +0600

    all done alhamdulillah. Now deploy and check

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ function getInputValue(inputIdName) {
     const inputNumber = parseFloat(getInput.value);
     /* Wrong input Validation */
     if ((getInput.value < 0 || isNaN(getInput.value) || getInput.value == '')) {
-        alert('Wrong input or empty field !!');
+        return alert('Wrong input or empty field !!');
     }
     return inputNumber;
 }
@@ -24,28 +24,36 @@ document.getElementById('btn-calculate').addEventListener('click', function () {
     const allCost = food + rent + clothes;
     document.getElementById('total-expenses').innerText = allCost;
     const totalExpense = getInnerTextNumber('total-expenses');
+    /* Validation whether total expenses amount can not exceed income amount */
+    if (totalExpense >= income) {
+        alert('Expenses exceeded income ');
+        document.getElementById('total-expenses').innerText = allCost;
+        document.getElementById('balance').innerText = 'Reduce expenses and try again';
+    }
+    else {
 
-    document.getElementById('balance').innerText = income - totalExpense;
-    document.getElementById('total-expenses').innerText = allCost;
+        document.getElementById('total-expenses').innerText = allCost;
+        document.getElementById('balance').innerText = income - totalExpense;
+    }
 }
 )
 // Event handler for save-button. This will calculate target saving inputs, display saving amount and remaining balance 
 document.getElementById('btn-save').addEventListener('click', function () {
     const income = getInputValue('input-income');
     const save = getInputValue('input-save');
-    const savingAmount = income * save / 100;
+    const savingAmount = income * save / 100; /* for saving percentage */
     document.getElementById('saving-amount').innerText = savingAmount;
     const balance = getInnerTextNumber('balance');
 
     /* Validation whether saving amount can not exceed balance amount */
-    if (balance >= savingAmount) {
-        document.getElementById('saving-amount').innerText = savingAmount;
-        document.getElementById('remaining-balance').innerText = balance - savingAmount;
-    }
-    else {
+    if (savingAmount >= balance) {
         alert('No enough balance to save');
         document.getElementById('saving-amount').innerText = "00";
         document.getElementById('remaining-balance').innerText = balance;
+    }
+    else {
+        document.getElementById('saving-amount').innerText = savingAmount;
+        document.getElementById('remaining-balance').innerText = balance - savingAmount;
 
     }
 }
