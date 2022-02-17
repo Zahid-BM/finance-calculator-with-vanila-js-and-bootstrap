31d7e625a6cc5827911724971d9e2b09d441cb69
BM. Zahidul Islam <bmzahidulislam@outlook.com>
2022-02-17 07:38:59 +0600

    all done Alhamdulillah

--- js/index.js
+++ js/index.js
@@ -1,16 +1,20 @@
+// function to get all input and convert into floating numbers
 function getInputValue(inputIdName) {
     const getInput = document.getElementById(inputIdName);
     const inputNumber = parseFloat(getInput.value);
+    /* Wrong input Validation */
     if (getInput.value < 0 || isNaN(getInput.value)) {
         alert('Wrong input !! Please check all input field and input correctly.');
     }
     return inputNumber;
 }
+// function to get innerText and convert into floating number
 function getInnerTextNumber(idName) {
     const innerText = document.getElementById(idName);
     const textToNumber = parseFloat(innerText.innerText);
     return textToNumber;
 }
+// Event handler for calculate-button. This will calculate all expenses inputs and display expenses total and balance amount 
 document.getElementById('btn-calculate').addEventListener('click', function () {
     const income = getInputValue('input-income');
     const food = getInputValue('input-food');
@@ -20,16 +24,30 @@ document.getElementById('btn-calculate').addEventListener('click', function () {
     document.getElementById('total-expenses').innerText = allCost;
     const totalExpense = getInnerTextNumber('total-expenses');
     const balance = getInnerTextNumber('balance');
-    document.getElementById('balance').innerText = income - totalExpense;
-    const save = getInputValue('input-save');
+    /* Validation whether expenses can not exceed income */
+    if (totalExpense <= income) {
+        document.getElementById('balance').innerText = income - totalExpense;
+    }
+    else {
+        alert('Total Expenses can not be higher than your income')
+        document.getElementById('balance').innerText = 'wrong';
+        document.getElementById('total-expenses').innerText = "Too Much Expenses.";
+    }
 })
+// Event handler for save-button. This will calculate target saving inputs, display saving amount and remaining balance 
 document.getElementById('btn-save').addEventListener('click', function () {
     const income = getInputValue('input-income');
     const save = getInputValue('input-save');
-    const saving = getInnerTextNumber('saving-amount');
-    const savingAmount = income / save;
-    // console.log(save);
+    const savingAmount = income * save / 100;
     document.getElementById('saving-amount').innerText = savingAmount;
     const balance = getInnerTextNumber('balance');
-    document.getElementById('remaining-balance').innerText = balance - savingAmount;
+    /* Validation whether saving amount can not exceed balance amount */
+    if (savingAmount < balance) {
+        document.getElementById('remaining-balance').innerText = balance - savingAmount;
+    }
+    else {
+        alert('No balance to save !!!')
+        document.getElementById('saving-amount').innerText = 'No Saving';
+        document.getElementById('remaining-balance').innerText = 'No Balance';
+    }
 })
